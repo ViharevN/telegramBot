1b0164f16d6c4f01c683b4130168e47ba6dcc8f9
ViharevN <swix_off@mail.ru>
2023-02-14 23:46:32 +0300

    first commit

---
+++ src/test/java/bot/sky/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.sky.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
