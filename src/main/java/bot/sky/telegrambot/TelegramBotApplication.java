e4f59751ad8e3e98a14784256d2397906c31f4e0
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-02-15 14:14:42 +0300

    В main добавил вывод в консоль сообщения "Start application"

--- src/main/java/bot/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/bot/sky/telegrambot/TelegramBotApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
+        System.out.println("Start application");
+
         SpringApplication.run(TelegramBotApplication.class, args);
     }
 
