7e1522834e6ed39ce870f1e86e7eeebc5a4fbb94
ViharevN <swix_off@mail.ru>
2023-02-28 00:18:42 +0300

    PostMapping add

--- src/main/java/bot/sky/telegrambot/service/controller/DogController.java
+++ src/main/java/bot/sky/telegrambot/service/controller/DogController.java
@@ -5,10 +5,7 @@ import bot.sky.telegrambot.models.Dog;
 import bot.sky.telegrambot.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,12 +22,16 @@ public class DogController {
     }
 
     @GetMapping("get/{id}")
-    public Dog getDogById(@PathVariable long id) {
-        if (!repository.existsById(id)) {
-            throw new DogNotFoundException("Dog not exist by id: " + id);
-        }
-        return repository.getById(id);
-
+    public ResponseEntity<Dog> getDogById(@PathVariable long id) {
+        Dog dog = repository.findById(id)
+                .orElseThrow(() -> new DogNotFoundException("Dog not found exist ID: " + id));
+        return ResponseEntity.ok().body(dog);
     }
+
+    @PostMapping("/add")
+    public Dog addDog(@RequestBody Dog dog) {
+        return repository.save(dog);
+    }
+
 
 }
