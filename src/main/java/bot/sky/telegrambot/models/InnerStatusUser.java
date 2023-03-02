51def944fb18f20e3ed7f6fcd35192c50d2b75b8
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-03-02 19:01:54 +0300

    Перереботал меню бота, переделал метод обработки сообщений в классе TelegramBot. Добавил класс - TestUserForMenu, чтобы переписать обработку сообщений и регистрацию пользователей (метод регистрации не сделан еще)

--- src/main/java/bot/sky/telegrambot/models/InnerStatusUser.java
+++ src/main/java/bot/sky/telegrambot/models/InnerStatusUser.java
@@ -1,2 +1,14 @@
-package bot.sky.telegrambot.models;public class InnerStatusUser {
+package bot.sky.telegrambot.models;
+
+public enum InnerStatusUser {
+    NEW_VISITOR("посетитель, который не выбрал тип приюта"),
+    NOT_REGISTERED_USER("посетитель, который выбрал тип приюта, но не прошел регистрацию"),
+    REGISTERED_USER("посетитель, который прошел регистрацию, но еще не взял питомца"),
+    USER_WITH_PET("посетитель, который прошел регистрацию, и взял взял питомца");
+
+    String aboutStatus;
+
+    InnerStatusUser(String aboutStatus) {
+        this.aboutStatus = aboutStatus;
+    }
 }
--- src/main/java/bot/sky/telegrambot/repository/TestUsersRepository.java
+++ src/main/java/bot/sky/telegrambot/repository/TestUsersRepository.java
@@ -2,6 +2,8 @@ package bot.sky.telegrambot.repository;
 
 import bot.sky.telegrambot.models.TestUserForMenu;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TestUsersRepository extends CrudRepository<TestUserForMenu, Long>{
 }
