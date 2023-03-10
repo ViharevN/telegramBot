aed25c94500aa1b4c1881f7c7aec593d4c71054c
Alexey <alexey_kuzminykh@mail.ru>
2023-03-10 14:31:36 +0300

    TelegramBotConfiguration

--- src/main/java/configuration/TelegramBotConfiguration.java
+++ src/main/java/configuration/TelegramBotConfiguration.java
@@ -1,7 +1,30 @@
 package configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.meta.api.methods.commands.DeleteMyCommands;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
 
+/**
+ * @author Alexey Kuzminykh
+ * @version 1.1.1
+ */
+@Configuration
+@EnableScheduling
 public class TelegramBotConfiguration {
+
+    @Value("6236177694:AAEmhn2TEUG8bWr2e0r2FVGlJipcbwTmfKI")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+
 
 
 }
