e2cc35b267a6eb1f7b3dfd7c943a2e0b4df94a23
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2023-03-11 16:10:00 +0300

    Добавил класс PersonDog и связанные с ним классы. Добавил enum Status Добавил зависимость lombok в pom.xml

---
+++ src/main/java/bot/sky/telegrambot/repository/PersonDogRepository.java
@@ -0,0 +1,15 @@
+package bot.sky.telegrambot.repository;
+
+import model.PersonDog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Set;
+
+
+/**
+ * Interface PersonDogRepository.   *
+ */
+@Repository
+public interface PersonDogRepository extends JpaRepository<PersonDog, Long> {
+    Set<PersonDog> findByChatId(Long chatId);
+}
